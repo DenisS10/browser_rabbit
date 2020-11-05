b6278ea5d267a0cf288375d210174c44b74a3979
DenisS <dasorokin@yahoo.com>
2020-11-05 16:32:43 +0300

    Intermediate

--- prepare-queues.php
+++ prepare-queues.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 //require_once 'Config.php';
 require_once 'RabbitMQ.php';
 
@@ -12,7 +13,7 @@ function actionPrepareQueues()
 
     $rabbitMQ = new RabbitMQ();
     $exchangeName = 'testing.hw.messages';
-    $queueName = $exchangeName; // . '.' . $_POST['tabUID'];
+    $queueName = $exchangeName . '.' . $_COOKIE['PHPSESSID'];
     $result = $rabbitMQ->createQueueAndBind(
         $queueName,
         [$exchangeName => $queueName],
